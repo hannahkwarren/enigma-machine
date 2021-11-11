3e48735407189a3119f3e1e7365f625b484ebcd3
Hannah Warren <hannahkwarren@gmail.com>
2021-11-11 11:05:51 -0500

    Add character set creation

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,6 +1,13 @@
 class Shift
 
-  def initialize
+  attr_reader :character_set
 
+  def initialize
+    @character_set = character_set
   end
+
+  def create_character_set
+    @character_set = ("a".."z").to_a << " "
+  end
+
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe Shift do
   it 'exists' do
     expect(@shift).to be_instance_of(Shift)
   end
+
+  it 'has attributes' do
+    expect(@shift.create_character_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
 end
