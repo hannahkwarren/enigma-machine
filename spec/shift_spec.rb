c623c700c382cce8682356a5d11ba7b6785292fe
Hannah Warren <hannahkwarren@gmail.com>
2021-11-11 13:38:36 -0500

    Add tests for random number generation, slicing key, square date

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Shift do
 
   before(:each) do
     @shift = Shift.new
+    @stub_key = '01234'
+    @stub_date = 111589
   end
 
   it 'exists' do
@@ -13,5 +15,17 @@ RSpec.describe Shift do
 
   it 'has attributes' do
     expect(@shift.create_character_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
+
+  it '#generate_random_number' do
+    allow(@shift.generate_random_number).to receive(:digits).and_return(@stub_key)
+  end
+
+  it '#slice_key' do
+    expect(@shift.slice_key(@stub_key)).to eq({:A => "01", :B => "12", :C => "23", :D => "34"})
+  end
+
+  it '#square_date' do
+    expect(@shift.square_date(@stub_date)).to eq(12452104921)
   end
 end
