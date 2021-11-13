5cbb7de53ab40db05489b4bc57a3a593b6740977
Hannah Warren <hannahkwarren@gmail.com>
2021-11-13 17:11:09 -0500

    Reorganization: remove shift class and spec files, put shift methods into enigma where they make more sense

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -2,7 +2,7 @@ require_relative './shift'
 
 module Encryptable
 
-  def _encrypt(offsets, current_shift, message, key, date)
+  def _encrypt(offsets, message, key, date)
 
     #all the symbols in the offsets hash, or :A, :B, :C, :D
     offset_keys = offsets.keys
@@ -16,9 +16,9 @@ module Encryptable
     #get the new character for each character in message
     message.each_char do |character|
       # if character exists in valid character_set...
-      if current_shift.character_set.include?(character)
+      if character_set.include?(character)
         # find the character index in the character_set
-        loc = current_shift.character_set.index(character)
+        loc = character_set.index(character)
         # from list of the symbols in offsets, get the current_offset to use
         offset_index = offset_keys[offset_counter % offset_keys.length]
         # use the current_offset to get the right number (value) in offsets
@@ -26,7 +26,7 @@ module Encryptable
         #increment the offset_counter
         offset_counter += 1
         #arithmetic for getting the new character
-        new = current_shift.character_set[(loc + current_offset) % current_shift.character_set.length]
+        new = character_set[(loc + current_offset) % character_set.length]
         #add new char to new string
         encrypted_text += new
       else
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,4 @@
-require_relative './shift'
+require 'securerandom'
 require_relative './encryptable'
 # require_relative './decryptable'
 
@@ -6,28 +6,71 @@ class Enigma
   include Encryptable
   # include Decryptable
 
-  attr_reader :key, :four_digits, :offsets
+  attr_reader :key, :four_digits, :offsets, :character_set
 
   def initialize
-    @current_shift = Shift.new
     @key = key
     @four_digits = four_digits
+    @character_set = character_set
   end
 
-  def accept_key(key)
-    @key = @current_shift.accept_existing_key(key)
+  def create_character_set
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def accept_key(given)
+    @key = given
   end
 
   def generate_key
-    @key = @current_shift.generate_random_number
+    # returns 5-digit random number between 00000 and 10**5,
+    # padding first digits as needed with zeros via .rjust
+    @key = SecureRandom.random_number(10**5).to_s.rjust(5, '0')
+  end
+
+  def use_today_date
+    today = Date.today
+    "#{today.month}#{today.mday}#{today.year.to_s[2,3]}"
+  end
+
+  def square_date(date)
+    int = date.to_i
+    square = int ** 2
+    square.to_s
   end
 
   def date_last_four(date)
-    @four_digits = @current_shift.date_last_four(date)
+    @four_digits = square_date(date)[-4..-1]
   end
 
-  def generate_offsets
-    @offsets = @current_shift.generate_offsets(key, four_digits)
+  def slice_key(key)
+    sliceable = key.clone
+
+    key_hash_keys = [:A, :B, :C, :D]
+    sliced_key = Hash.new
+
+    key_hash_keys.each do |symbol|
+      #slice off and return the first 2 characters of key
+      sliced_key[symbol] = sliceable.slice!(0..1)
+      #then put back the second character at the beginning of leftover
+      #  chars, so it can be included as first character for the next slice
+      sliceable.prepend(sliced_key[symbol][1])
+    end
+    sliced_key.map {|k, v| [k, v.to_i]}.to_h
+  end
+
+  def slice_date
+    date_hash_keys = [:A, :B, :C, :D]
+    sliced_date = Hash.new
+
+    date_hash_keys.each do |symbol|
+      sliced_date[symbol] = four_digits.slice!(0)
+    end
+    sliced_date.map {|k, v| [k, v.to_i]}.to_h
+  end
+
+  def generate_offsets(key, four_digits)
+    @offsets = slice_key(key).merge!(slice_date){ |key, key_slice, date_slice| key_slice + date_slice}
   end
 
   def downcase_message(message)
@@ -39,25 +82,26 @@ class Enigma
     if provided_key.class == String && provided_key.length == 6
       provided_date = provided_key
       provided_key = nil
-    end
-
-    message = downcase_message(message)
-    @current_shift.create_character_set
-
-    if provided_key == nil
       self.generate_key
     else
       self.accept_key(provided_key)
     end
 
+    self.create_character_set
+    message = downcase_message(message)
+
+    if provided_key == nil
+      self.generate_key
+    end
+
     if provided_date == nil
-      provided_date = @current_shift.use_today_date
+      provided_date = self.use_today_date
     end
 
     self.date_last_four(provided_date)
-    self.generate_offsets
+    self.generate_offsets(key, four_digits)
 
-    text = _encrypt(@offsets, @current_shift, message, provided_key, provided_date)
+    text = _encrypt(@offsets, message, key, provided_date)
     encrypted_text = {encryption: text,
       key: key,
       date: provided_date}
